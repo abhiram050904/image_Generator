34c1dd5d1c462ea6a430513158436e698b9880a8
abhiram050904 <abhiram050904@gmail.com>
2024-07-19 14:14:21 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
+import styled, { ThemeProvider } from "styled-components";
+import {darkTheme} from "./utils/theme"
+import { formHelperTextClasses } from "@mui/material";
+import Home from './pages/home';
+import Createpost from "./pages/createpost";
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import Navbar from "./components/navbar";
+const Container=styled.div`
+width:100%;
+height:100%;
+display:flex
+background-color:${({theme})=> theme.bg};
+color: ${({theme})=>theme.text_primary};
+overflow-x:hidden;
+overflow-y:hidden;
+transition:all 0.2:
+`;
+
+const Wrapper=styled.div`
+height:100%
+position:relative
+display:flex
+flex-direction:column
+justify-content:space-between
+flex:3
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ThemeProvider theme={darkTheme}>
+    <Container>
+      <Wrapper>
+        <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} exact/>
+          <Route path='/post' element={<Createpost />} exact/>
+        </Routes>
+        </BrowserRouter>
+      </Wrapper>
+    </Container>
+    </ThemeProvider>
 }
 
 export default App;
